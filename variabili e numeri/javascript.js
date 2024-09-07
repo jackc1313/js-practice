25abefdebf4787312eced3e955e72fab8e2bbdb4
jackc1313 <jacopo.cariato98@gmail.com>
2024-09-07 19:33:12 +0200

    creato un file .js
    ho deciso di spostare lo script su un file separato dall'index e ho continuato a lavorarci su

---
+++ variabili e numeri/javascript.js
@@ -0,0 +1,36 @@
+console.log("Hello, World!")
+//la funzione di questo comando e' di stampare qualcosa sulla developer console. posso vederne il risultato aprendo questo file sul browser e aprendo la console.
+    
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+//ho assegnato un nuovo valore alla variabile gia' dichiarata in precedenza
+
+const pi = 3.14;
+//const e' un tipo di variabile che non viene riassegnata
+//pi = 10;
+console.log(pi);
+/*se provo a riassegnare la variabile, nella console viene visualizzato un errore
+gli errori nella console vanno bene perche' ci permettono di capire dove dobbiamo lavorare*/
+
+console.log((3 + 2) - 76 * (1 + 1));
+//viene stampato il risultato di questa espressione sulla console
+
+console.log((4 + 6 + 9)/77);
+
+let a = 10;
+let b = 7 * a;
+console.log(b);
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+//ho stampato il valore di percentage scrivendo "percentage" direttamente nella console
