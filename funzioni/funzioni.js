3903367f64edaa75c119585676263fcee615fb98
jackc1313 <jacopo.cariato98@gmail.com>
2024-09-08 18:07:11 +0200

    funzioni e arrow functions

---
+++ funzioni/funzioni.js
@@ -0,0 +1,29 @@
+function animalePreferito(animale) { //nelle tonde, il PARAMETRO
+    return animale + " e\' il mio animale preferito"
+}
+console.log(animalePreferito("La scimmia")); //nelle tonde, l'ARGOMENTO
+
+//funzione per generare un numero intero a caso
+function random(numero) {
+    return Math.floor(Math.random() * numero)
+}
+console.log(random(19));
+/* sto continuando a usare console.log per 'invocare' le funzioni,
+ma in realt' per invocarle bisogna scriverne il nome seguito dalle
+parentesi*/
+
+//arrow function: =>
+let somma = (a, b) => a + b;
+console.log(somma(2, 3));
+//arrow function per moltiplicare numeri in un array
+let numeri = [1, 2, 3, 4, 5];
+let numeriDoppi = numeri.map(numero => numero * 2);
+console.log(numeriDoppi);
+//.map() itera un'operazione su tutti i numeri dell'array
+
+let add7 = (a) => a + 7;
+console.log(add7(5));
+
+let moltiplica = (a, b) => a * b;
+console.log(moltiplica(2, 3));
+
