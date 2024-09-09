99a4b38c19daff74c0a992dd8a2af6b25e65f8fc
jackc1313 <jacopo.cariato98@gmail.com>
2024-09-09 18:33:50 +0200

    aggiunto commento

--- ciclo-for/for.js
+++ ciclo-for/for.js
@@ -3,7 +3,7 @@
 con la parola 'CIAO', fino al numero inserito dall'utente*/
 let numero = parseInt(prompt('Inserisci il numero', ""));
 for (let i = 1; i <= numero; i++) {
-    if (i % 3 === 0) {
+    if (i % 3 === 0) { //divisore che restituisce il resto -> in questo caso deve essere zero
         console.log('CIAO');
     } else {
         console.log(i);
