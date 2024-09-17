d330622d3f3a3bb460e8d900b2ca091ea12a2372
jackc1313 <jacopo.cariato98@gmail.com>
2024-09-17 17:38:47 +0200

    aggiunto piu' bottoni con querySelectorAll

--- button/javascript.js
+++ button/javascript.js
@@ -2,3 +2,14 @@ const btn = document.querySelector("#btn");
 btn.addEventListener("click", () => { 
     alert("Ciao!");
 });
+
+const buttons = document.querySelectorAll("button"); /* 'buttons'
+e' un 'node list' con quesrySelectoAll, funziona un po' come un array*/
+
+//il metodo .forEach itera tra tutti i bottoni
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        alert(button.id);
+  });
+});
+//all'interno della funzione aggiungiamo un event listener per ogni bottone
